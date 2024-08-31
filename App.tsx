11aee35eb7523d43380e6ac181b793134103fa35
dhlananhh <dhlananh2309@gmail.com>
2024-08-31 22:17:39 +0700

    update files & add assets

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+    return (
+        <div>Home</div>
+    );
+};
+
+export default App;
